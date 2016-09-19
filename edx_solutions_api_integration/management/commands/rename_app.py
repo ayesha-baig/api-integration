597150d451a523e9e12f4d61aa233630237417a5
Ayesha Baig <ayesha.baig@arbisoft.com>
2016-09-19 11:35:42 +0500

    modified to add new function

--- edx_solutions_api_integration/management/commands/rename_app.py
+++ edx_solutions_api_integration/management/commands/rename_app.py
@@ -10,6 +10,11 @@ from django.apps import apps
 log = logging.getLogger(__name__)
 
 
+def get_app_model_names(app_name):
+    app = apps.get_app_config(app_name)
+    return app.models
+
+
 class Command(BaseCommand):
     """
     Renames an app from old name to new name and updates database accordingly
@@ -30,15 +35,17 @@ class Command(BaseCommand):
         new_app_name = options['new_app_name']
 
         cursor = connection.cursor()
+
         with transaction.atomic():
-            cursor.execute("UPDATE django_content_type SET app_label = %s WHERE app_label = %s",
+            cursor.execute('UPDATE django_content_type SET app_label = %s WHERE app_label = %s',
                            [new_app_name, old_app_name])
 
-            # Get the old app's models to rename tables in database
-            old_app = apps.get_app_config(old_app_name)
-            for old_table_name in old_app.models:
-                new_table_name = new_app_name + "_" + old_table_name
-                old_table_name = old_app_name + "_" + old_table_name
+            # Get the names of old app's models to rename tables in database
+            old_app_model_names = get_app_model_names(old_app_name)
+
+            for model_name in old_app_model_names:
+                new_table_name = new_app_name + '_' + model_name
+                old_table_name = old_app_name + '_' + model_name
 
                 sql = 'ALTER TABLE %s RENAME TO %s' % (old_table_name, new_table_name)
                 cursor.execute(sql)
